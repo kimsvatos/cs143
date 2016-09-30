f4eae8645f3ead8a8bb0fc15a5026368c64ec6f5
kimsvatos <kimsvatos@ucla.edu>
2016-09-29 20:25:21 -0700

    parses spaces

--- calculator.php
+++ calculator.php
@@ -1,1 +1,27 @@
-//this is our project wheheeeeeee
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_POST['fname'];
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+        $name = str_replace(" ", "", $name);
+        echo $name;
+    }
+}
+?>
+
+</body>
+</html>
+
+
+
