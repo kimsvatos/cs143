2d5c349bcf8e993e128f9478cad285e7c63e8a25
kimsvatos <kimsvatos@ucla.edu>
2016-09-30 18:29:35 -0700

    working calc

--- calculator.php
+++ calculator.php
@@ -1,22 +1,47 @@
 <html>
 <body>
-
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
+<h1>Kim And Kyle's Calcultor!!!!11!1</h1>
+<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <input type="text" name="nums">
+  <input type="submit" value="Calculate">
 </form>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     // collect value of input field
-    $name = $_POST['fname'];
-    if (empty($name)) {
-        echo "Name is empty";
+    $eq = $_GET['nums'];
+    if (empty($eq)) {
+        
     } else {
+        
+        $eq = str_replace(" ", "", $eq);
+        $subject = $eq;
+        $pattern = "/[^0123456789+-\/\.\*]/";
+        $zeroCheck = strpos($eq, "/0");
+        if($zeroCheck){
+            echo "<h2>Result</h2>";       
+            exit("Division by zero error!");
+        }
+       preg_match($pattern, $subject, $matches);
 
-        $name = str_replace(" ", "", $name);
-        echo $name + 1;
-        $multPos = strpos($name, "*");
+       if(empty($matches)){ 
+              eval("\$result = $eq;");
+              $pattern = "/[+-\/\*\.][+\/\*\.]/";
+              preg_match($pattern, $subject, $matches);
+              if(empty($matches)){
+                   echo "<h2>Result</h2>";
+                   echo $eq . " = " . $result; 
+              }
+              else{
+                  echo "<h2>Result</h2>";
+                  echo "Invalid Expression!";
+              }
+       
+        }
+        else{
+        echo "<h2>Result</h2>";
+        echo "Invalid Expression!";
+        }
     }
 }
 ?>
