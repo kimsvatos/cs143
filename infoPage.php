7f70b28c326559855bc804b6b9071a0cf051a491
kimsvatos <kimsvatos@ucla.edu>
2016-10-28 16:33:10 -0700

    kyle is dumb

--- infoPage.php
+++ infoPage.php
@@ -3,11 +3,12 @@
 		<title> I hate my job </title>
 	</head>
 	<body>
+		<marquee>
 		<h1> Hola Mundo! </h1>
 		Welcome to Kim + Kyles endless adventure website of movies and actors!!!<br>
 		Plz click any link below to navigate to some rad places, dude!
 
-		<br><marquee behavior="scroll"> 
+		<br>
 		Kyle + Kim
 		<form method="post" action="test.php">
        		 <input type="Submit" value="Insert a New Actor/Director">
