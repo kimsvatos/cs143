c8b4e1ccd536beab174c1623937d6c45d4d11c12
kimsvatos <kimsvatos@ucla.edu>
2016-10-28 16:54:16 -0700

    adding stuff

--- movieInfo.php
+++ movieInfo.php
@@ -25,22 +25,22 @@
  	  Adult<input type="checkbox" name="adult" value="ON">
       Adventure<input type="checkbox" name="adventure" value="ON">
  	  Animation<input type="checkbox" name="animation" value="ON">
- 	  Comedy<input type="checkbox" name="comedy" value="ON">
+ 	  Comedy<input type="checkbox" name="comedy" value="ON"><br>
  	  Crime<input type="checkbox" name="crime" value="ON">
       Documentary<input type="checkbox" name="documentary" value="ON">
  	  Drama<input type="checkbox" name="drama" value="ON">
- 	  Family<input type="checkbox" name="family" value="ON">
+ 	  Family<input type="checkbox" name="family" value="ON"><br>
  	  Fantasy<input type="checkbox" name="fantasy" value="ON">
       Horror<input type="checkbox" name="horror" value="ON">
  	  Musical<input type="checkbox" name="musical" value="ON">
- 	  Mystery<input type="checkbox" name="mystery" value="ON">
+ 	  Mystery<input type="checkbox" name="mystery" value="ON"><br>
  	  Romance<input type="checkbox" name="romance" value="ON">
       Sci-Fi<input type="checkbox" name="scifi" value="ON">
  	  Short<input type="checkbox" name="short" value="ON">
- 	  Thriller<input type="checkbox" name="thriller" value="ON">
+ 	  Thriller<input type="checkbox" name="thriller" value="ON"><br>
  	  War<input type="checkbox" name="war" value="ON">
       Western<input type="checkbox" name="western" value="ON">
- 	  
+ 	  <br>
       <input type="submit" value="Submit">
    </form>
 	</body>
