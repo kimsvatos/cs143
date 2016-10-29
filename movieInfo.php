439a02163992d4c460954d9330e97011b4ca4643
kimsvatos <kimsvatos@ucla.edu>
2016-10-29 14:04:28 -0700

    adding stuff

--- movieInfo.php
+++ movieInfo.php
@@ -28,7 +28,7 @@
       </SELECT>
 
       <br>
-      <marquee speed=100>Action<input type="checkbox" name="action" value="ON"></marquee>
+      Action<input type="checkbox" name="action" value="ON">
  	  Adult<input type="checkbox" name="adult" value="ON">
       Adventure<input type="checkbox" name="adventure" value="ON">
  	  Animation<input type="checkbox" name="animation" value="ON">
@@ -37,12 +37,12 @@
       Documentary<input type="checkbox" name="documentary" value="ON">
  	  Drama<input type="checkbox" name="drama" value="ON">
  	  Family<input type="checkbox" name="family" value="ON"><br>
- 	  <marquee speed=200>Fantasy<input type="checkbox" name="fantasy" value="ON">
+ 	  Fantasy<input type="checkbox" name="fantasy" value="ON">
       Horror<input type="checkbox" name="horror" value="ON">
- 	  Musical<input type="checkbox" name="musical" value="ON"></marquee>
+ 	  Musical<input type="checkbox" name="musical" value="ON">
  	  Mystery<input type="checkbox" name="mystery" value="ON"><br>
- 	  <marquee speed=1>Romance<input type="checkbox" name="romance" value="ON">
-      Sci-Fi<input type="checkbox" name="scifi" value="ON"></marquee>
+ 	  Romance<input type="checkbox" name="romance" value="ON">
+      Sci-Fi<input type="checkbox" name="scifi" value="ON">
  	  Short<input type="checkbox" name="short" value="ON">
  	  Thriller<input type="checkbox" name="thriller" value="ON"><br>
  	  War<input type="checkbox" name="war" value="ON">
@@ -50,6 +50,72 @@
  	  <br>
       <input type="submit" value="Submit">
    </form>
+   <?php
+   		if ($_SERVER["REQUEST_METHOD"] == "GET"){
+        $quitError=0;
+      # CHECK that we have valid input
+        if (!($movieTitle = $_GET['title'])) {
+          echo "Need a movie title!<br>";
+          $quitError=1;
+        }
+        if (!($movieCo = $_GET['company'])) {
+          echo "Need a movie company!<br>";
+          $quitError=1;
+        }
+       
+        $year = $_GET['year'];
+        $horror = $_GET['horror'];
+        $drama = $_GET['drama'];
+
+        echo "$drama<br>";
+        echo "$horror";
+
+
+
+        if(!$quitError){
+          #where we would want to INSERT
+          $database = new mysqli('localhost', 'cs143', '', 'CS143');
+          if($database->connect_errno > 0){
+            die('Unable to connect to database [' . $database->connect_error . ']');
+          }
+          //make sure to do query to get max person ID and then increment that too
+          $id_query = "SELECT * FROM MaxPersonID;";
+          $MaxPersonID = $database->query($id_query);
+          $result = $MaxPersonID->fetch_array(MYSQLI_NUM);
+          $id = $result[0] + 1;
+
+          #director query
+          $occupation = $_GET['actor_director'];
+          if($occupation == "Director"){
+            $personStatement = $database->prepare("INSERT INTO Director (id, last, first, dob, dod) VALUES (?, ?, ?, ?, ?);");
+            $personStatement->bind_param("issss", $id, $lastName, $firstName, $dob, $dod);
+          }
+          #actor query
+          else{
+            $personStatement = $database->prepare("INSERT INTO Actor (id, last, first, sex, dob, dod) VALUES (?, ?, ?, ?, ?, ?);");
+            $personStatement->bind_param("isssss", $id, $lastName, $firstName, $sex, $dob, $dod);
+          }
+         
+          $personStatement->execute();
+          //TODO: make sure INSERT succeeded before updating this value
+
+          $id_statement = $database->prepare("UPDATE MaxPersonID SET id = $id;");
+          $id_statement->execute();
+
+         
+          echo "submitted!";
+        }
+      }
+
+
+
+   ?>
+
+
+
+
+
+
 	</body>
 
 
