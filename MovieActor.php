3cc0a4e8ec2b8705bbff5744268eb278568e5bae
kimsvatos <kimsvatos@ucla.edu>
2016-10-29 16:05:24 -0700

    added movieActor

--- MovieActor.php
+++ MovieActor.php
@@ -7,12 +7,24 @@
     }
     $movieQuery = $database->query("SELECT id, title, year FROM Movie order by title;");
     $movieList = "";
-    $movieList = $movieList . "<option value=\"id\"> Select Movie </option>";
+    $movieList = $movieList . "<option SELECTED value=\"-1\"> Select Movie </option>";
     while($row = $movieQuery->fetch_array(MYSQLI_ASSOC)){
     	$id = $row["id"];
     	$title = $row["title"];
      	$year = $row["year"];
-    	$movieList = $movieList . "<option value=\"id\"> $title ($year) </option>";
+    	$movieList = $movieList . "<option value=\"$id\"> $title ($year) </option>";
+
+    }
+
+    $actorQuery = $database->query("SELECT id, last, first, dob FROM Actor order by last;");
+    $actorList = "";
+    $actorList = $movieList . "<option value=\"-1\"> Select Actor</option>";
+    while($row = $movieQuery->fetch_array(MYSQLI_ASSOC)){
+    	$last = $row["last"];
+    	$id = $row["id"];
+    	$first = $row["first"];
+     	$dob = $row["dob"];
+    	$movieList = $movieList . "<option value=\"$id\"> $last $first ($dob)</option>";
 
     }
 
@@ -21,6 +33,9 @@
 <form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
 	<SELECT name="movie">
 		<?=$movieList?>
+	</SELECT>
+	<SELECT name="actor">
+		<?=$actorList?>
 	</SELECT>
 
 </form>
