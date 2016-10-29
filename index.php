4fb6b2825bdb05cb1d462ce91fead6ecdbd24343
kimsvatos <kimsvatos@ucla.edu>
2016-10-29 16:46:23 -0700

    linking

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-jkfljsdkjdsfljkfekljjfkljfdsakljdfslkdsflksdfklhi
+<html>
+	<head>
+		<title> CS143 Lab 1C </title>
+	</head>
+	<body>
+		
+		<h1> Hola Mundo! </h1>
+		Welcome to Kim + Kyles endless adventure website of movies and actors!!!<br>
+		Plz click any link below to navigate to some rad places, dude!
+
+		<br>
+		Kyle + Kim
+		<form method="post" action="test.php">
+       		 <input type="Submit" value="Insert a New Actor/Director">
+    	</form>
+    	<form method="post" action="movieInfo.php">
+       		 <input type="Submit" value="Insert a New Movie">
+    	</form>
+    	<form method="post" action="movieActor.php">
+       		 <input type="Submit" value="Add a Movie/Actor relationship!">
+    	</form>
+    	<form method="post" action="MovieDirector.php">
+       		 <input type="Submit" value="Add a Movie/Director relationship!">
+    	</form>
+    	
+
+	</body>
+
+</html>
