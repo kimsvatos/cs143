e34a9fe617db7308f972b8b42910f2588981f86a
kimsvatos <kimsvatos@ucla.edu>
2016-10-29 14:36:44 -0700

    movegenre testing

--- movieInfo.php
+++ movieInfo.php
@@ -81,7 +81,9 @@
      	  $rating = $_GET['rating'];
 
           if(($genre = $_GET['action']) == "ON"){
+          	echo "$genre<br>";
         	$genreStatement = $database->prepare("INSERT INTO MovieGenre (mid, genre) VALUES (?, ?);");
+        	$genre = "Action";
          	$genreStatement->bind_param("is", $id, $genre);
          	$genreStatement->execute();
          	echo "success!<br>";
