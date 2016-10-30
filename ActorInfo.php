49b04d367389894edf1da8adae7f6aee61e8b4a0
kimsvatos <kimsvatos@ucla.edu>
2016-10-30 16:30:32 -0700

    debug

--- ActorInfo.php
+++ ActorInfo.php
@@ -9,7 +9,7 @@
 	if ($_SERVER["REQUEST_METHOD"] == "GET"){
 		
 		$name = $_GET['search'];
-		//$name = "%" . $name . "%";
+		$name = "%" . $name . "%";
 
 		$database = new mysqli('localhost', 'cs143', '', 'CS143');
     	if($database->connect_errno > 0){
@@ -17,19 +17,25 @@
    		}
    		//"kyle haacker"
    		//$query = "SELECT id, first, last, dob FROM Actor where last like $name or first like $name;";
-   		$query = "SELECT * FROM Actor order by last";
-        $result = $database->query($query);
+   		//$query = "SELECT * FROM Actor order by last";
+   		$rStatement = $database->prepare("SELECT id, first, last, dob FROM Actor where last like ? or first like ?;");
+        //$result = $database->query($query);
+        $rStatement->bind_param("ss", $name, $name);
+        $rStatement->execute();
+        $rStatement->bind_result($r_id, $r_first, $r_last, $r_dob);
+
+
         echo "<table border='1' bordercolor='black' cellpadding='2'>";
         echo "<tr>";
         echo "<th>Actor Name</th>";
         echo "<th>Date of Birth</th>";
         echo "</tr>";
         echo "hello";
-        while($iter = $result->fetch_array(MYSQLI_ASSOC)){
+        while($rStatement->fetch()){
         	echo "hi";
-        	$name = $iter['first'] . " " . $iter['last'];
-        	$dob = $iter['dob'];
-        	$id = $iter['id'];
+        	$name = $r_first . " " . $r_last;
+        	$dob = $r_dob;
+        	$id = $r_id;
 
         	echo "<tr>";
         	echo "<td> $name </td>";
