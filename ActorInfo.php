2248a334c886c04bd0075f508dd4ffa07937e4c0
kimsvatos <kimsvatos@ucla.edu>
2016-10-30 16:11:25 -0700

    add actor info

---
+++ ActorInfo.php
@@ -0,0 +1,45 @@
+<html>
+<h1> Actor Information!</h1>
+<body>
+	<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+		<input type="text" size="80" name="search">
+		<input type="Submit" value="Search Actors!">
+	</form>
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "GET"){
+		
+		$name = $_GET['search'];
+		$name = "%" . $name . "%";
+
+		$database = new mysqli('localhost', 'cs143', '', 'CS143');
+    	if($database->connect_errno > 0){
+   	    	 die('Unable to connect to database [' . $database->connect_error . ']');
+   		}
+   		$query = "SELECT id, first, last, dob FROM Actor where last like $name or first like $name;";
+        $result = $database->query($query);
+        echo "<table border='1' bordercolor='black' cellpadding='2'>";
+        echo "<tr>";
+        echo "<th>Actor Name</th>";
+        echo "<th>Date of Birth</th>";
+        echo "</tr>";
+        while($iter= $result->fetch_array(MYSQLI_ASSOC)){
+        	
+        	$name = $iter['first'] . " " . $iter['last'];
+        	$dob = $iter['dob'];
+        	$id = $iter['id'];
+
+        	echo "<tr>";
+        	echo "<td> $name </td>";
+        	echo "<td> $dob </td>";
+        	echo "</tr>";
+        }
+
+
+
+	}
+?>
+
+
+
+</body>
+</html>
