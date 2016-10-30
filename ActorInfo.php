43f709297a71b7a6f17e3d02c7a203fa303c70e2
kimsvatos <kimsvatos@ucla.edu>
2016-10-30 16:49:53 -0700

    debug

--- ActorInfo.php
+++ ActorInfo.php
@@ -9,20 +9,33 @@
 	if ($_SERVER["REQUEST_METHOD"] == "GET"){
 		
 		$name = $_GET['search'];
-		$name = "%" . $name . "%";
+		$words = explode(' ', $name);
+		
 
 		$database = new mysqli('localhost', 'cs143', '', 'CS143');
     	if($database->connect_errno > 0){
    	    	 die('Unable to connect to database [' . $database->connect_error . ']');
    		}
    		//"kyle haacker"
-   		//$query = "SELECT id, first, last, dob FROM Actor where last like $name or first like $name;";
+   		$i=0;
+   		$query = "SELECT id, first, last, dob FROM Actor";
+   		while($i < count($words)){
+   			if($i == 0){
+   				$query = $query . " WHERE";
+   			}
+   			else{
+   				$query = $query . " and"
+   			}
+   		$query = $query . " (last like '%$words[i]%' or first like '%$words[i]%')";
    		//$query = "SELECT * FROM Actor order by last";
-   		$rStatement = $database->prepare("SELECT id, first, last, dob FROM Actor where last like ? or first like ?;");
-        //$result = $database->query($query);
-        $rStatement->bind_param("ss", $name, $name);
-        $rStatement->execute();
-        $rStatement->bind_result($r_id, $r_first, $r_last, $r_dob);
+   		//$rStatement = $database->prepare("SELECT id, first, last, dob FROM Actor where last like ? or first like ?;");
+        
+        }
+        $query = $query . ";";
+    	$result = $database->query($query);
+        //$rStatement->bind_param("ss", $name, $name);
+        //$rStatement->execute();
+        //$rStatement->bind_result($r_id, $r_first, $r_last, $r_dob);
 
 
         echo "<table border='1' bordercolor='black' cellpadding='2'>";
@@ -31,11 +44,14 @@
         echo "<th>Date of Birth</th>";
         echo "</tr>";
         echo "hello";
-        while($rStatement->fetch()){
+        //while($rStatement->fetch()){
+        while($row = $result->fetch_array(MYSQLI_ASSOC)){}
         	echo "hi";
-        	$name = $r_first . " " . $r_last;
-        	$dob = $r_dob;
-        	$id = $r_id;
+
+        ///fix 
+        	$name = $row['first'] . " " . $row['last'];
+        	$dob = $row['dob'];
+        	$id = $row['id'];
 
         	echo "<tr>";
         	echo "<td> $name </td>";
