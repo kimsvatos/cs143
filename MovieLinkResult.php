948a7001150b41aaf565b6bfc875abb7425d359f
kimsvatos <kimsvatos@ucla.edu>
2016-10-30 17:51:33 -0700

    this is a shit show

---
+++ MovieLinkResult.php
@@ -0,0 +1,75 @@
+<html>
+<body>
+
+
+<h1>Movie RESULTUTLTLSDJLSD</h1>
+<!-- do queries and show table with DOB name DOD blah --> 
+<?php
+	$id = $_GET['id'];
+	$database = new mysqli('localhost', 'cs143', '', 'CS143');
+    if($database->connect_errno > 0){
+        die('Unable to connect to database [' . $database->connect_error . ']');
+    }
+
+  
+    $id_query = "SELECT first, last, dob, sex, dod FROM Actor WHERE id=$id;";
+    $queryRes = $database->query($id_query);
+    $result = $queryRes->fetch_array(MYSQLI_ASSOC);
+    echo $result['last'];
+    echo " <h1> Actor Information is: </h1>";
+    echo "<table border='1' bordercolor='black' cellpadding='2'>";
+    echo "<tr>";
+    echo "<th>Actor Name</th>";
+    echo "<th>Sex</th>";
+    echo "<th>Date of Birth</th>";
+    echo "<th>Date of Death</th>";
+    echo "</tr>";
+
+    //add info below
+    echo "<tr>";
+    echo "<td>" . $result['first'] . " " . $result['last'] . "</td>";
+	echo "<td>" . $result['sex'] . "</td>";
+	echo "<td>" . $result['dob'] . "</td>";
+	if($result['dod'] == NULL){
+		echo "<td> N/A </td>";
+	}
+	else{
+		echo "<td>" . $result['dod'] . "</td>";
+	}
+	echo "</tr>";
+	echo "</table>";
+
+	//movie info 
+	$id_query = "SELECT role, title from MovieActor, Movie WHERE aid = $id AND mid=id";
+    $queryRes = $database->query($id_query);
+    
+	///next table
+    echo "<h1> Actor's Movies and Role:</h1>";
+
+
+    echo "<table border='1' bordercolor='black' cellpadding='2'>";
+    echo "<tr>";
+    echo "<th>Movie Title</th>";
+    echo "<th>Role</th>";
+   
+    echo "</tr>";
+
+    while($result = $queryRes->fetch_array(MYSQLI_ASSOC)){
+    echo "<tr>";
+    echo "<td>" . $result['title'] . "</td>"; //this will link to MOVIElinkResult
+	echo "<td>" . $result['role'] . "</td>";
+	
+	echo "</tr>";
+	}	
+
+	echo "</table>";
+   // echo "</table>";
+?>
+	<form method="post" action="ActorInfo.php">
+        <input type="Submit" value="Go back to make another search!">
+    </form>
+
+</body>
+
+
+</html>
---
+++ MovieSearchPage.php
@@ -0,0 +1,76 @@
+<html>
+<h1> Actor Information!</h1>
+<body>
+	<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+		<input type="text" size="80" name="search">
+		<input type="Submit" value="Search Actors!">
+	</form>
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "GET"){
+		
+		$name = $_GET['search'];
+		$words = explode(' ', $name);
+		echo "$words[0]<br>";
+		echo "$words[1]<br>";
+		$database = new mysqli('localhost', 'cs143', '', 'CS143');
+    	if($database->connect_errno > 0){
+   	    	 die('Unable to connect to database [' . $database->connect_error . ']');
+   		}
+   		//"kyle haacker"
+   		$i=0;
+   		$query = "SELECT id, title, year FROM Movie";
+   		echo count($words);
+   		while($i < count($words)){
+   			echo "$words[$i] is in row $i<br>";
+   			if($i == 0){
+   				$query = $query . " WHERE";
+   			}
+   			else{
+   				$query = $query . " and";
+   			}
+   		   $query = $query . " (title like '%$words[$i]%')";
+   		//$query = "SELECT * FROM Actor order by last";
+   		//$rStatement = $database->prepare("SELECT id, first, last, dob FROM Actor where last like ? or first like ?;");
+	       $i++;
+        }
+        $query = $query . ";";
+    	$result = $database->query($query);
+        //$rStatement->bind_param("ss", $name, $name);
+        //$rStatement->execute();
+        //$rStatement->bind_result($r_id, $r_first, $r_last, $r_dob);
+
+
+        echo "<table border='1' bordercolor='black' cellpadding='2'>";
+        echo "<tr>";
+        echo "<th>Title</th>";
+        echo "<th>Year</th>";
+        echo "</tr>";
+        echo "hello";
+        //while($rStatement->fetch()){
+        $i = 0;
+        while($row = $result->fetch_array(MYSQLI_ASSOC)){
+        	echo "$i";
+        	$i++;
+        ///fix 
+        	$title = $row['title'];
+        	$year = $row['year'];
+        	$id = $row['id'];
+        	
+
+
+        	echo "<tr>";
+        	echo "<td> <a href=\"MovieLinkResult.php?id=".$row["id"]."\">".$title."</a></td>";
+        	echo "<td> <a href=\"MovieLinkResult.php?id=".$row["id"]."\">".$year."</a> </td>";
+        	echo "</tr>";
+        }
+        echo "</table>";
+
+
+
+	}
+?>
+
+
+
+</body>
+</html>
