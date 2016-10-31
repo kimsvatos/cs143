9216bf9ae43cbf8d82fba2b8df5aaee1ad5ed7a2
kimsvatos <kimsvatos@ucla.edu>
2016-10-30 17:37:59 -0700

    try to get id

--- LinkResult.php
+++ LinkResult.php
@@ -12,11 +12,41 @@
     }
 
     echo "$id";
-    $id_query = "SELECT first, last, dob, dod FROM Actor WHERE id=$id;";
+    $id_query = "SELECT first, last, dob, sex, dod FROM Actor WHERE id=$id;";
     $queryRes = $database->query($id_query);
     $result = $queryRes->fetch_array(MYSQLI_ASSOC);
     echo $result['last'];
+    echo " <h1> Actor Information is: </h1>";
+    echo "<table border='1' bordercolor='black' cellpadding='2'>";
+    echo "<tr>";
+    echo "<th>Actor Name</th>";
+    echo "<th>Sex</th>";
+    echo "<th>Date of Birth</th>";
+    echo "<th>Date of Death</th>";
+    echo "</tr>";
+
+    //add info below
+    echo "<tr>";
+    echo "<td>" . $result['first'] . " " . $result['last'] . "</td>";
+	echo "<td>" . $result['sex'] . "</td>";
+	echo "<td>" . $result['dob'] . "</td>";
+	if($result['dod'] == NULL){
+		echo "<td> N/A </td>";
+	}
+	else{
+		echo "<td>" . $result['dod'] . "</td>";
+	}
+	echo "</tr>";
+	echo "</table>";
+
+
+	///next table
+    echo "<h1> Actor's Movies and Role:</h1>";
+   // echo "</table>";
 ?>
+	<form method="post" action="ActorInfo.php">
+        <input type="Submit" value="Go back to make another search!">
+    </form>
 
 </body>
 
