c906c79aa115964298ef7e8f8f4cef6d9d45d1f6
kimsvatos <kimsvatos@ucla.edu>
2016-10-30 17:27:14 -0700

    try to get id

--- LinkResult.php
+++ LinkResult.php
@@ -2,10 +2,17 @@
 <body>
 
 
-<h1>CONGRATS</h1>
+<h1>Information</h1>
 <!-- do queries and show table with DOB name DOD blah --> 
+<?php
+	$id = $_GET['id'];
+	$database = new mysqli('localhost', 'cs143', '', 'CS143');
+    if($database->connect_errno > 0){
+        die('Unable to connect to database [' . $database->connect_error . ']');
+    }
 
-
+    echo "$id";
+?>
 
 </body>
 
