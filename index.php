d91d1e5cbfa28f1472909e54e34e68e5f93f1982
kimsvatos <kimsvatos@ucla.edu>
2016-11-01 18:22:00 -0700

    attempt pretty

--- index.php
+++ index.php
@@ -1,15 +1,63 @@
 <html>
-	<head>
-		<title> CS143 Lab 1C </title>
-	</head>
-	<body>
-		
-		<h1> Hola Mundo! </h1>
-		Welcome to Kim + Kyles endless adventure website of movies and actors!!!<br>
-		Plz click any link below to navigate to some rad places, dude!
 
-		<br>
-		Kyle + Kim
+
+
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+    border-right:1px solid #bbb;
+}
+
+li:last-child {
+    border-right: none;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #4CAF50;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="./test.php">Insert a New Actor/Director</a></li>
+  <li><a href="./movieInfo.php">Insert a New Movie</a></li>
+  <li><a href="./movieActor.php">Add a Movie/Actor relationship!</a></li>
+  <li><a href="./movieDiretor.php">Add a Movie/Director relationship!</a></li>
+  <li><a href="./review.php">Add a Review</a></li>
+  <li><a href="./ActorInfo.php">Actor Lookup</a></li>
+  <li><a href="./MovieSearchPage.php">Movie Lookup</a></li>
+  <li><a href="./GenSearch.php">General Search</a></li>
+  <li style="float:right"><a href="#about">About</a></li>
+</ul>
+
+</body>
+</html>
+
+		<!--<br>
+		Kyle + Kim-->
+    <!--
 		<form method="post" action="test.php">
        		 <input type="Submit" value="Insert a New Actor/Director">
     	</form>
@@ -33,8 +81,8 @@
       </form>
       <form method="post" action="GenSearch.php">
            <input type="Submit" value="Make a general serach!">
-      </form>
+      </form>-->
 
-	</body>
+	<!--</body>
 
-</html>
+</html>-->
