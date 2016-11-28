ec2b8c340fc8d0583ba455460c49ffd36c0301a2
System Administrator <root@HAACKERK-2.local>
2016-11-27 19:33:44 -0800

    wrapping up select

--- bruinbase/SqlEngine.cc
+++ bruinbase/SqlEngine.cc
@@ -43,48 +43,61 @@ RC SqlEngine::select(int attr, const string& table, const vector<SelCond>& cond)
   RC     rc;
   int    key;     
   string value;
-  int    count;	// TODO: don't forgot to initialize where appropriate...
+  int    count;
   int    diff;
 
-  /* New variables, added for our implementation of B+ tree  */
-  BTreeIndex   bti;
-  IndexCursor  cur;
-  const int    ATTR_KEY = 1;
-  const int    ATTR_VAL = 2;
-  const int    ATTR_ALL = 3;
-  const int    ATTR_CNT = 4;
-  SelCond      currCond;
-  int          currCompValue;
-  bool         useIndex  = false;
-  bool         needValue = false;
-  int          minKey = -1;
-  bool		   minGT = false;  // true means key is > min (false means >= min)
-  int          maxKey = -1;
-  bool         maxLT = false;  // true means key is < max (false means <= max)
+  /* The following are all new variables, added for our implementation of B+ tree    */
 
+  /* Variables for accessing index file  										     */
+  BTreeIndex   bti;					 /* The index, which we access when appropriate  */
+  IndexCursor  cur;				/* Cursor used to read consecutive entries in index  */
+  int          searchKey;          /* The key we will initially search for in index  */
+  bool         indexOpen = false;       /* Tells us whether we need to close at end  */
 
+  /* Constants that describe what attributes are returned in query (more readable)   */
+  const int    ATTR_KEY = 1;			                  /* Query returns key only  */
+  const int    ATTR_VAL = 2;							/* Query returns value only  */
+  const int    ATTR_ALL = 3;					  	 /* Query returns key and value  */
+  const int    ATTR_CNT = 4;    /* Query returns # tuples satisfying all conditions  */
 
+  /* Numerous variables to track all WHERE conditions, and if they make sense        */
+  SelCond      currCond;						 /* Current condition being checked  */
+  int          currCompValue;	/* Current compare value of condition being checked  */
   
+  bool         useIndex  = false;	   /* Does it make sense to use index for query  */
+  bool         needValue = false;	  /* Will we need to get value when reading rf?  */
+  
+  int          minKey = -1;				      /* What is minimum key value allowed?  */
+  bool		   minKeySet = false;     		 /* Has a minimum key been set in cond?  */
+  bool		   minKeyGT = false;  	  /* True means key must be > minKey (false >=)  */
+  
+  int          maxKey = -1;					  /* What is maximum key value allowed?  */
+  bool         maxKeySet = false;			 /* Has a maximum key been set in cond?  */
+  bool         maxKeyLT = false;  	  /* True means key must be < maxKey (false <=)  */
+  
+  int          keyEQValue = -1;				    /* What must key value be equal to?  */
+  bool         keyEQValueSet = false;	         /* Has key value been set in cond?  */
+  
+  string 	   valEQValue = "";					/* What must str value be equal to?  */
+  bool		   valEQValueSet = false;		     /* Has str value been set in cond?  */
+
+
   /* Open table file to retrieve query results  */
   if ((rc = rf.open(table + ".tbl", 'r')) < 0) {
     fprintf(stderr, "Error: table %s does not exist\n", table.c_str());
     return rc;
   }
 
-  /* If we cannot open the index file (likely doesn't exist), we
-   * have to perform the select by scanning through each tuple.  */
-  if (bti.open(table + ".idx", 'r'))
-  	goto no_index_select;
+  /////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
 
-
-
-  /* TODO: Begin index stuff here...  */
-  //////////////////////////////////////
-  //////////////////////////////////////
-
+  /* Before doing any reads of RecordFile or index PageFile, check validity of all
+   * conditions in the WHERE clause and determine if index should be used for query. */
   for (unsigned i = 0; i < cond.size(); i++) {
 
+  	/* Obtain current condition and int-representation of value to compare against   */
   	currCond = cond[i];
+  	currCompValue = atoi(currCond.value);
 
   	/* The current condition is on the "key" attribute  */
   	if (currCond.attr == ATTR_KEY) {
@@ -95,71 +108,267 @@ RC SqlEngine::select(int attr, const string& table, const vector<SelCond>& cond)
   			continue;
   		}
 
-  		/* Otherwise, comp is not NE, which suggests we should use index for select.
-  		 * Additionally, retrieve the key comparison value as an int.  */
+  		/* Otherwise, comp is not NE, which suggests we should use index for select.  */
   		useIndex = true;
-  		currCompValue = atoi(currCond.value);
-
-
+  		
+  		/* Depending on current comparison (EQ, LT, GT, LE, GE), correctly set the
+  		 * appropriate condition variables and check for any invalidities.  */
   		switch (currCond.comp) {
   			
+  			/* There can only be 1 equality on a key. If there are multiple equalities,
+  			   we can finish now because no tuple will satisify the conditions.  */
   			case SelCond::EQ:
-  				;		// TODO Do not blindly follow, consider casee of multiple EQ
+  				if ((!keyEQValueSet) || (keyEQValue == currCompValue)) {
+  					keyEQValue    = currCompValue;
+  					keyEQValueSet = true;
+  				}
+  				else
+  					goto end_of_select;
   				break;
 
+  			/* If a LT value is already defined, the condition variable only changes
+  			 * if we have found a smaller (or equal) LT value  */
   			case SelCond::LT:
   				if ((maxKey == -1) || (currCompValue <= maxKey)) {
-  					maxKey  = currCompValue;
-  					maxLT   = true;
+  					maxKey    = currCompValue;
+  					maxKeyLT  = true;
+  					maxKeySet = true;
   				}
   				break;
 
+  			/* If a GT value is already defined, the condition variable only changes
+  			 * if we have found a larger (or equal) GT value  */
   			case SelCond::GT:
   				if ((minKey == -1) || (currCompValue >= minKey)) {
-  					minKey = currCompValue;
-  					minGT  = true;
+  					minKey    = currCompValue;
+  					minKeyGT  = true;
+  					minKeySet = true;
   				}
   				break;
 
+  			/* If a LE value is already defined, the condition variable only changes
+  			 * if we have found a smaller LE value  */
   			case SelCond::LE:
   				if ((maxKey == -1) || (currCompValue < maxKey)) {
-  					maxKey  = currCompValue;
-  					maxLT   = false;
+  					maxKey    = currCompValue;
+  					maxKeyLT  = false;
+  					maxKeySet = true;
   				}
   				break;
 
+  			/* If a GE value is already defined, the condition variable only changes
+  			 * if we have found a larger GE value  */
   			case SelCond::GE:
   				if ((minKey == -1) || (currCompValue > minKey)) {
-  					minKey  = currCompValue;
-  					minGT   = false;
+  					minKey    = currCompValue;
+  					minKeyGT  = false;
+  					minKeySet = true;
   				}
   				break;
 
   			default:
   				break;
   		}
-
   	}
   	
   	/* The current condition is on the "value" attribute  */
   	else if (currCond.attr == ATTR_VAL) {
 
+  		/* Similar for the key attribute, there can only be 1 equality on a value.
+  		 * If there are multiple, we can finish now; no attributes will be returned.  */
+  		switch (currCond.comp) {
+  			case SelCond::EQ:
+  				if ((!valEQValueSet) || (strcmp(valEQValue.c_str(), cond[i].value) == 0)) {
+  					valEQValue = string(cond[i].value);
+  					valEQValueSet = true;
+  				}
+  				else
+  					goto end_of_select;
+  			default:
+  				needValue = true;
+  				break;
+  		}
+
   	}
 
-    /* We shouldn't end up here, condition must be on key or value  */
+    /* We shouldn't end up here, condition must be on key or value attribute  */
   	else
   		continue;  
+  }
 
+  /////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
+
+  /* Finished going through all conditions, time to check them. If we can, we'll take
+   * shortcuts to avoid page reads. Also, we'll decide if we should use index.  */
+
+  /* e.g. select * from table where key>5 and key<3  */
+  if (minKeySet && maxKeySet && (minKey > maxKey))
+  	goto end_of_select;
+
+  /* e.g. select * from table where key<=5 and key>5  */
+  if (minKeySet && maxKeySet && (minKey == maxKey) && (minKeyGT || maxKeyLT))
+  	goto end_of_select;
+
+  /* e.g. select * from table where key=5 and key<5  */
+  if (keyEQValueSet && maxKeySet && (maxKey == keyEQValue) && maxKeyLT)
+  	goto end_of_select;
+
+  /* e.g. select * from table where key=5 and key>5  */
+  if (keyEQValueSet && minKeySet && (minKey == keyEQValue) && minKeyGT)
+  	goto end_of_select;
+
+  /* e.g. select * from table where key=5 and key<=4  */
+  if (keyEQValueSet && maxKeySet && (maxKey < keyEQValue))
+  	goto end_of_select;
+
+  /* e.g. select * from table where key=5 and key>6  */
+  if (keyEQValueSet && minKeySet && (minKey > keyEQValue))
+  	goto end_of_select;
+
+  /* If we cannot open the index file (likely doesn't exist), we
+   * have to perform the select by scanning through each tuple.  */
+  if (bti.open(table + ".idx", 'r'))
+  	goto no_index_select;
+  else
+  	indexOpen = true;
+
+  if (!useIndex)	// TODO: Not sure about adding "&& attr != ATTR_CNT"
+  	goto no_index_select;
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
+  
+  /* If we have not jumped, we will now perform an index-based select here  */
+  rid.pid = rid.sid = 0;
+  count = 0;
+
+  /* If there is an equality, we should search directly for that key  */
+  if (keyEQValueSet)
+  	searchKey = keyEQValue;
+
+  /* Else, if we know the min key value, we can skip all those keys < (or <=) minKey  */
+  else if (minKeySet)
+  	searchKey = (minKeyGT ? minKey+1 : minKey);
+  
+  /* Otherwise, start with the smallest possible int value to start at first tuple (+1 to avoid warning)  */
+  else
+  	searchKey = -2147483647;
+
+  /* Once our search key is decided, go to the correct location on leaf level to begin reading.
+   * Continue to perform 'readForward' on tuples until they will no longer satisfy conditions.  */
+  bti.locate(searchKey, cur);
+  while (bti.readForward(cur, key, rid) == 0) {
+
+  	// TODO: Not sure about this...
+  	if (!needValue && (attr == ATTR_CNT)) {
+
+  		if (keyEQValueSet && keyEQValue != key)
+  			goto end_of_select;  		
+
+  		if (minKeySet) {
+  			if ((minKeyGT && key <= minKey) || (!minKeyGT && key < minKey))
+  				goto end_of_select;
+  		}
+
+  		if (maxKeySet) {
+  			if ((maxKeyLT && key >= maxKey) || (!maxKeyLT && key > maxKey))
+  				goto end_of_select;
+  		}
+
+  		count++;
+  		goto next_tuple_index;
+  	}
+
+  	/* Read the record, just like in the pre-existing select code  */
+  	if (rc = rf.read(rid, key, value) < 0) {
+  		fprintf(stderr, "Error: while reading a tuple from table %s\n", table.c_str());
+  		goto exit_select;
+  	}
+
+  	/* The remaining code is very similar to that for the pre-existing select code (see no_index_select:).
+  	   Modifications that we've made have been commented.  */
+  	for (unsigned i = 0; i < cond.size(); i++) {
+
+  		switch (cond[i].attr) {
+  			case 1:
+  				diff = key - atoi(cond[i].value);
+  				break;
+  			case 2:
+  				diff = strcmp(value.c_str(), cond[i].value);
+  				break;
+  		}
+
+  		switch (cond[i].comp) {
+
+  			/* If we are dealing with keys, a diff!=0 indicates the key will never again be equivalent to the
+  			 * comparison key, meaning the select is finished. For values, continue scanning tuples.  */
+  			case SelCond::EQ:
+  				if (diff != 0) {
+  					if (cond[i].attr == ATTR_KEY) goto end_of_select;
+  					else                          goto next_tuple_index;
+  				}
+  				break;
+
+  			/* Nothing's changed here.  */
+  			case SelCond::NE:
+  				if (diff == 0) goto next_tuple_index;
+  				break;
+
+  			/* Nothing's changed here.  */
+  			case SelCond::GT:
+  				if (diff <= 0) goto next_tuple_index;
+  				break;
+
+  			/* If we are dealing with keys, a diff>=0 indicates a key will never again be less than the
+  			 * comparison key, meaning the select is finished. For values, continue scanning tuples.  */
+  			case SelCond::LT:
+  				if (diff >= 0) {
+  					if (cond[i].attr == ATTR_KEY) goto end_of_select;
+  					else                          goto next_tuple_index;
+  				}
+  				break;
+
+  			/* Nothing's changed here.  */
+  			case SelCond::GE:
+  				if (diff < 0) goto next_tuple_index;
+  				break;
+
+  			/* If we are dealing with keys, a diff>0 indicates a key will never again be less than or
+  			 * equal to the comparison key, meaning select is finished. For values, continue scanning.  */
+  			case SelCond::LE:
+  				if (diff > 0) {
+  					if (cond[i].attr == ATTR_KEY) goto end_of_select;
+  					else                          goto next_tuple_index;
+  				}
+  				break;
+  		}
+  	}
+
+  	// the condition is met for the tuple.
+  	// increasing matching tuple counter 
+  	count++;
+
+    // print the tuple 
+    switch (attr) {
+    	case 1:  // SELECT key
+      		fprintf(stdout, "%d\n", key);
+      		break;
+    	case 2:  // SELECT value
+      		fprintf(stdout, "%s\n", value.c_str());
+      		break;
+    	case 3:  // SELECT *
+      		fprintf(stdout, "%d '%s'\n", key, value.c_str());
+      		break;
+    }
+  	
+  	next_tuple_index: ;	/* No-op, go to next tuple  */
   }
 
   goto end_of_select;
 
-  ///////////////////////////////////////////
-  ///////////////////////////////////////////
-  /* END TODO: End of added index stuff... */
-
-
-
+  /////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
 
   /* This is the select code that was already written for us. It scans through all tuples
    * and checks if each tuple satisifies the condition, printing it if it does. We perform
@@ -237,19 +446,23 @@ RC SqlEngine::select(int attr, const string& table, const vector<SelCond>& cond)
 	  /* At the end of no_index_select, go to end_of_select code  */
 	  goto end_of_select;
 
+  /////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
 
-  /* This code should be executed after performing select.  */
   end_of_select:
-	  // print matching tuple count if "select count(*)"
-	  if (attr == 4) {
-	    fprintf(stdout, "%d\n", count);
-	  }
-	  rc = 0;
+
+  // print matching tuple count if "select count(*)"
+  if (attr == 4) {
+    fprintf(stdout, "%d\n", count);
+  }
+  rc = 0;
+
+  exit_select:
 
   /* Close the table file, the index file (if applicable), and return.  */
-  exit_select:
-	  rf.close();
-	  return rc;
+  rf.close();
+  if (indexOpen) bti.close();
+  return rc;
 }
 
 RC SqlEngine::load(const string& table, const string& loadfile, bool index)
